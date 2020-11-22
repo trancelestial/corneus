e663011a159b15609e79eb94f9a78960275c0075
trancelestial <anticboz@gmail.com>
2020-11-22 19:05:56 +0100

    Save and load model

--- code/base/base_model.py
+++ code/base/base_model.py
@@ -1,11 +1,13 @@
 from abc import ABC, abstractclassmethod
 from shutil import Error
 from trainer import Trainer
+import torch
 
 class BaseModel(object):
     def __init__(self, device='cuda', **kwargs):
         super().__init__()
         self.device = device
+        self.net = None
         self.trainer = None
 
     def train(self, train_data, val_data=None, loss_name='bce', optimizer_name='adam', lr=1e-4,
@@ -26,6 +28,16 @@ class BaseModel(object):
 
         print(f'\n[TEST] Loss: {test_loss} Acc: {test_acc}')
     
+
+    def save_model(self, path):
+        assert self.net is not None, 'First initialize/train your model!'
+        torch.save(self.model.net.state_dict(), path)
+        print(f'Model saved to {path}')
+
+    def load_model(self, path):
+        assert self.net is not None, 'First initialize your model!'
+        self.net.load_state_dict(torch.load(path))
+        print(f'Model load from {path}')
 
     # def predict(self, data, batch_size=None):
     #     loader = torch.utils.data.DataLoader(data, batch_size=batch_size, shuffle=False)
