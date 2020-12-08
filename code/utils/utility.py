356ca0ed9e2f7a0bec98e4bac3db3bb5803c457f
trancelestial <anticboz@gmail.com>
2020-12-08 19:31:46 +0100

    Small changes (still needs bunch of refactoring)

--- code/utils/utility.py
+++ code/utils/utility.py
@@ -1,6 +1,8 @@
 import numpy as np
 import shutil
 import os
+import torch
+from torch.utils.data import DataLoader
 
 def generate_data_folders(base_path, typ, img_paths, labels, split=None):
     # split - percentage of validation data
@@ -21,3 +23,12 @@ def generate_data_folders(base_path, typ, img_paths, labels, split=None):
             wr_path = os.path.join(base_path, typ)
 
         shutil.move(os.path.join(base_path, typ, img_paths[i]), os.path.join(wr_path, labels[i], img_paths[i]))
+
+def calculate_normalization(inspect_data, batch_size=128, shuffle=False):
+    imgs = []
+    inspect_loader = DataLoader(inspect_data, batch_size=128, shuffle=True)
+    for img, _ in inspect_loader:
+        imgs.append(img)
+    ti = torch.cat(imgs, axis=0)
+    
+    return torch.mean(ti, dim=(0,2,3)), torch.std(ti, dim=(0,2,3)) 
